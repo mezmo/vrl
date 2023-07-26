093162fb96fad1a53ec62c6072e8d51803d7119b
Michael Penick <112402428+penick@users.noreply.github.com>
2023-07-26 09:32:27 -0400

    feat: Add functions to coerce int and float types (#28)
    This adds functions that can be used by the JS processor to coerce types
    into integers and floats attempting to follow the JavaScript conversion
    rules.
    
    Also, this limits the types that `mezmo_parse_float()` and
    `mezmo_parse_int()` handle to better match the functionality of
    `parseFloat()` and `parseInt()`.
    
    Ref: LOG-17443

---
+++ lib/stdlib/src/mezmo_to_float.rs
@@ -0,0 +1,141 @@
+use ::value::Value;
+use vrl::prelude::*;
+use vrl_core::{ExpressionError, Resolved};
+
+fn to_float(value: Value) -> Resolved {
+    use Value::{Boolean, Bytes, Float, Integer, Null, Timestamp};
+    match value {
+        Float(_) => Ok(value),
+        Integer(v) => Ok(Value::from_f64_or_zero(v as f64)),
+        Boolean(v) => Ok(NotNan::new(if v { 1.0 } else { 0.0 }).unwrap().into()),
+        Null => Ok(NotNan::new(0.0).unwrap().into()),
+        Timestamp(v) => Ok(Value::from_f64_or_zero(
+            v.timestamp_nanos() as f64 / 1_000_000_000_f64,
+        )),
+        Bytes(v) => {
+            let s = String::from_utf8_lossy(&v);
+            let parsed = s.trim().parse::<f64>().map_err(|e| {
+                <std::string::String as std::convert::Into<ExpressionError>>::into(e.to_string())
+            })?;
+            Ok(NotNan::new(parsed)
+                .map_err(|_| {
+                    <&str as std::convert::Into<ExpressionError>>::into("NaN is not supported")
+                })?
+                .into())
+        }
+        v => Err(format!("unable to coerce {} into float", v.kind()).into()),
+    }
+}
+
+/// Same as the stdlib `to_float()`, but fallibility is determined at runtime
+/// (there are no `type_def()` checks) and it trims whitespace.
+#[derive(Clone, Copy, Debug)]
+pub struct MezmoToFloat;
+
+impl Function for MezmoToFloat {
+    fn identifier(&self) -> &'static str {
+        "mezmo_to_float"
+    }
+
+    fn parameters(&self) -> &'static [Parameter] {
+        &[Parameter {
+            keyword: "value",
+            kind: kind::ANY,
+            required: true,
+        }]
+    }
+
+    fn examples(&self) -> &'static [Example] {
+        &[]
+    }
+
+    fn compile(
+        &self,
+        _state: &state::TypeState,
+        _ctx: &mut FunctionCompileContext,
+        arguments: ArgumentList,
+    ) -> Compiled {
+        let value = arguments.required("value");
+
+        Ok(ToFloatFn { value }.as_expr())
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ToFloatFn {
+    value: Box<dyn Expression>,
+}
+
+impl FunctionExpression for ToFloatFn {
+    fn resolve(&self, ctx: &mut Context) -> Resolved {
+        let value = self.value.resolve(ctx)?;
+        to_float(value)
+    }
+
+    fn type_def(&self, _state: &state::TypeState) -> TypeDef {
+        TypeDef::float().fallible()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    test_function![
+        mezmo_to_float => MezmoToFloat;
+
+        float {
+            args: func_args![value: 20.5],
+            want: Ok(20.5),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        integer {
+            args: func_args![value: 100],
+            want: Ok(100.0),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        string {
+            args: func_args![value: "100.1"],
+            want: Ok(100.1),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        string_whitespace {
+            args: func_args![value: "    100.1  "],
+            want: Ok(100.1),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        string_whitespace_neg_exp {
+            args: func_args![value: "    1e-3  "],
+            want: Ok(0.001),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        string_whitespace_pos_exp {
+            args: func_args![value: "    1e+9  "],
+            want: Ok(1000000000.0),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        null {
+            args: func_args![value: value!(null)],
+            want: Ok(0.0),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        array {
+            args: func_args![value: value!([])],
+            want: Err("unable to coerce array into float"),
+            tdef: TypeDef::float().fallible(),
+        }
+
+        object {
+            args: func_args![value: value!({})],
+            want: Err("unable to coerce object into float"),
+            tdef: TypeDef::float().fallible(),
+        }
+    ];
+}
