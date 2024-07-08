7b20ff8c30a39de423d72596c8ccdc51aca1f193
Kwabena Boadu <shadrack.boadu@gmail.com>
2024-07-08 00:42:46 -0400

    feat(mezmo_matching_patterns): Add mezmo_matching_patterns function
    The function allows a caller to identify patterns
    which were found in a given data stream.
    
    Ref: LOG-20230

---
+++ src/stdlib/mezmo_matching_patterns.rs
@@ -0,0 +1,271 @@
+use super::mezmo_patterns::{
+    CREDIT_CARD_PATTERN, EMAIL_ADDRESS_PATTERN, IPV4_ADDRESS_PATTERN, PHONE_NUMBER_PATTERN,
+    US_SOCIAL_SECURITY_NUMBER_PATTERN,
+};
+use crate::compiler::prelude::*;
+use regex::bytes::RegexSet;
+use std::collections::BTreeSet;
+
+fn matching_patterns(value: Value, pattern: &RegexSet) -> Resolved {
+    match value {
+        Value::Bytes(bytes) => Ok(Value::Array(
+            pattern
+                .matches(&bytes)
+                .into_iter()
+                .map(Value::from)
+                .collect(),
+        )),
+        Value::Array(values) => process_collection(values.into_iter(), pattern),
+        Value::Object(map) => process_collection(map.into_iter().map(|(_, value)| value), pattern),
+        _ => Ok(Value::Array(vec![])),
+    }
+}
+
+fn process_collection(values: impl Iterator<Item = Value>, pattern: &RegexSet) -> Resolved {
+    let mut matches: BTreeSet<i64> = BTreeSet::new();
+    // skips recursing into nested elements if all patterns are matched in previous paths
+    for value in values {
+        matching_patterns(value, pattern)?
+            .try_array()?
+            .into_iter()
+            .map(|value| {
+                value
+                    .try_integer()
+                    .expect("matched pattern item must be integer")
+            })
+            .for_each(|index| {
+                matches.insert(index);
+            });
+
+        if matches.len() == pattern.len() {
+            break;
+        }
+    }
+    Ok(Value::Array(matches.into_iter().map(Value::from).collect()))
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct MatchingPatterns;
+
+impl Function for MatchingPatterns {
+    fn identifier(&self) -> &'static str {
+        "mezmo_matching_patterns"
+    }
+
+    fn parameters(&self) -> &'static [Parameter] {
+        &[
+            Parameter {
+                keyword: "value",
+                kind: kind::BYTES | kind::OBJECT | kind::ARRAY,
+                required: true,
+            },
+            Parameter {
+                keyword: "patterns",
+                kind: kind::ARRAY,
+                required: true,
+            },
+        ]
+    }
+
+    fn examples(&self) -> &'static [Example] {
+        &[
+            Example {
+                title: "match on string",
+                source: r#"matching_patterns("foobar", patterns: [r'\w+', r'\d+', r'\pL+', r'foo', r'bar', r'barfoo', r'foobar'])"#,
+                result: Ok("[[0, 2, 3, 4, 6]"),
+            },
+            Example {
+                title: "match on object",
+                source: r#"matching_patterns({"name": "jon doe", "department": "sales", "notes": {"entry" => "foobar works"}}, patterns: [r'\w+', r'\d+', r'\pL+', r'foo', r'bar', r'barfoo', r'foobar'])"#,
+                result: Ok("[[0, 2, 3, 4, 6]"),
+            },
+            Example {
+                title: "match on object",
+                source: r#"matching_patterns(["something", "strange", {"notes": {"entry" => "foobar works"}}], patterns: [r'\w+', r'\d+', r'\pL+', r'foo', r'bar', r'barfoo', r'foobar'])"#,
+                result: Ok("[[0, 2, 3, 4, 6]"),
+            },
+            Example {
+                title: "no match",
+                source: r#"match_any("My name is John Doe", patterns: [r'\d+', r'Jane'])"#,
+                result: Ok("[]"),
+            },
+        ]
+    }
+
+    fn compile(
+        &self,
+        state: &state::TypeState,
+        _ctx: &mut FunctionCompileContext,
+        arguments: ArgumentList,
+    ) -> Compiled {
+        let value = arguments.required("value");
+
+        let re_strings = arguments
+            .required_array("patterns")?
+            .into_iter()
+            .map(|expr| {
+                expr.resolve_constant(state)
+                    .ok_or(function::Error::ExpectedStaticExpression {
+                        keyword: "patterns",
+                        expr,
+                    })
+            })
+            .map(|value| {
+                value.and_then(|value| {
+                    let err = function::Error::InvalidArgument {
+                        keyword: "patterns",
+                        value: value.clone(),
+                        error: "unknown literal for pattern, must be a regex or pattern name",
+                    };
+                    if let Value::Bytes(bytes) = value.clone() {
+                        match bytes.as_ref() {
+                            b"credit_card" => Ok(CREDIT_CARD_PATTERN.to_string()),
+                            b"email" => Ok(EMAIL_ADDRESS_PATTERN.to_string()),
+                            b"ipv4" => Ok(IPV4_ADDRESS_PATTERN.to_string()),
+                            b"phone_number" => Ok(PHONE_NUMBER_PATTERN.to_string()),
+                            b"us_social_security_number" => {
+                                Ok(US_SOCIAL_SECURITY_NUMBER_PATTERN.to_string())
+                            }
+                            _ => Err(err),
+                        }
+                    } else {
+                        value
+                            .try_regex()
+                            .map_err(|_| err)
+                            .and_then(|re| Ok((*re).to_string()))
+                    }
+                })
+            })
+            .collect::<std::result::Result<Vec<String>, _>>()?;
+
+        let regex_set = RegexSet::new(re_strings).expect("regex were already valid");
+
+        Ok(MatchingPatternsFn { value, regex_set }.as_expr())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct MatchingPatternsFn {
+    value: Box<dyn Expression>,
+    regex_set: RegexSet,
+}
+
+impl FunctionExpression for MatchingPatternsFn {
+    fn resolve(&self, ctx: &mut Context) -> Resolved {
+        let value = self.value.resolve(ctx)?;
+        matching_patterns(value, &self.regex_set)
+    }
+
+    fn type_def(&self, _: &state::TypeState) -> TypeDef {
+        TypeDef::array(Collection::from_unknown(Kind::integer())).infallible()
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::trivial_regex)]
+mod tests {
+    use super::*;
+    use crate::value;
+    use regex::Regex;
+
+    test_function![
+        matching_patterns => MatchingPatterns;
+
+        string_value {
+            args: func_args![value: "foobar",
+                             patterns: vec![
+                                 Regex::new("foo").unwrap(),
+                                 Regex::new("baz").unwrap(),
+                                 Regex::new("bar").unwrap(),
+                                 Regex::new("baz|quux").unwrap(),
+                                 Regex::new("foobar").unwrap(),
+                             ]],
+            want: Ok(value!([0, 2, 4])),
+            tdef: TypeDef::array(Collection::from_unknown(Kind::integer())).infallible(),
+        }
+
+        object_value {
+            args: func_args![
+                value: value!({
+                    "name": "ben johnson",
+                    "age": 20,
+                    "manager": {
+                        "name": "jane thomson",
+                        "title": "senior director",
+                        "reportees": [
+                            {
+                                "name": "luka doncic",
+                                "team": "dallas mavericks"
+                            },
+                            {
+                                "name": "michael jordan",
+                                "team": "chicago bulls"
+                            },
+                        ]
+                    }
+                }),
+                patterns: vec![
+                    Regex::new("jordan").unwrap(),
+                    Regex::new("\\d{3}-\\d{2}").unwrap(),
+                    Regex::new("\\w+@\\w.com").unwrap(),
+                    Regex::new("luka").unwrap(),
+                ]],
+            want: Ok(value!([0, 3])),
+            tdef: TypeDef::array(Collection::from_unknown(Kind::integer())).infallible(),
+        }
+
+        array_value {
+            args: func_args![
+                value: value!([
+                    "welcome to the NBA",
+                    "list of employees below",
+                    {
+                        "dallas": {
+                            "coaches": ["jason kidd", "jared dudley"],
+                            "players": ["luka", "kyrie"]
+                        }
+                    },
+                    ["curry has 5 rings", "klay thomson is joining dallas"]
+                ]),
+                patterns: vec![
+                    Regex::new("jordan").unwrap(),
+                    Regex::new("\\d{3}-\\d{2}").unwrap(),
+                    Regex::new("luka").unwrap(),
+                    Regex::new("klay|jason").unwrap(),
+                    Regex::new("\\w+@\\w.com").unwrap(),
+                ]],
+            want: Ok(value!([2, 3])),
+            tdef: TypeDef::array(Collection::from_unknown(Kind::integer())).infallible(),
+        }
+
+        no_match {
+            args: func_args![
+                value: value!([
+                    "welcome to the NBA",
+                    "list of employees below",
+                    {
+                        "dallas": {
+                            "coaches": ["jason kidd", "jared dudley"],
+                            "players": ["luka", "kyrie"]
+                        }
+                    },
+                    ["curry has 5 rings", "klay thomson is joining dallas"]
+                ]),
+                patterns: vec![
+                    Regex::new("\\d{3}-\\d{2}").unwrap(),
+                    Regex::new("\\w+@\\w.com").unwrap(),
+                ]],
+            want: Ok(value!([])),
+            tdef: TypeDef::array(Collection::from_unknown(Kind::integer())).infallible(),
+        }
+
+        ssn_match {
+            args: func_args![
+                value: "my SSN is 122-33-4444",
+                patterns: vec!["credit_card", "email", "us_social_security_number", "phone_number"],
+            ],
+            want: Ok(value!([2])),
+            tdef: TypeDef::array(Collection::from_unknown(Kind::integer())).infallible(),
+        }
+    ];
+}
--- src/stdlib/mezmo_patterns.rs
+++ src/stdlib/mezmo_patterns.rs
@@ -1,8 +1,12 @@
+// https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s12.html
+// (converted to non-lookaround version given `regex` does not support lookarounds)
+// See also: https://www.ssa.gov/history/ssn/geocard.html
 pub(crate) const US_SOCIAL_SECURITY_NUMBER_PATTERN: &str = r#"(?x)                                                               # Ignore whitespace and comments in the regex expression.
     \b(?:00[1-9]|0[1-9][0-9]|[1-578][0-9]{2}|6[0-57-9][0-9]|66[0-57-9])[-\s]    # Area number: 001-899 except 666
     (?:0[1-9]|[1-9]0|[1-9][1-9])[-\s]                                         # Group number: 01-99
     (?:000[1-9]|00[1-9]0|0[1-9]00|[1-9]000|[1-9]{4})\b                      # Serial number: 0001-9999
     "#;
+// Patterns taken from: https://github.com/logdna/logdna-agent-v2/blob/master/docs/REGEX.md
 pub(crate) const EMAIL_ADDRESS_PATTERN: &str = r#"(?x)
     (?i:[a-z0-9!\#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!\#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
     "#;
